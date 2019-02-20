e14d57376515ddfea4b8102c5ecb101e0573b485
Nicholas Morrow <njmorrow95@gmail.com>
2019-02-19 21:30:25 -0500

    minor

--- colors.js
+++ colors.js
@@ -1,8 +1,8 @@
 module.exports = {
-	blue:		' #26BBD9',
-	cyan:		' #59E1E3',
-	green:		' #29D398',
-	magenta:	' #EE64AC',
-	red:		' #E95678',
-	yellow:		' #FAB795'
+	blue: " #26BBD9",
+	cyan: " #59E1E3",
+	green: " #29D398",
+	magenta: " #EE64AC",
+	red: " #E95678",
+	yellow: " #FAB795"
 };
