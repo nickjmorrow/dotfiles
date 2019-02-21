a4b9c8324877328d76887d7501d8c6b8064f2d16
Nicholas Morrow <nmorrow@predictivetechnologies.com>
2019-02-21 17:32:34 -0500

    these shouldn't change

--- colors.js
+++ colors.js
@@ -1,8 +1,16 @@
 module.exports = {
-	blue: " #26BBD9",
-	cyan: " #59E1E3",
-	green: " #29D398",
-	magenta: " #EE64AC",
-	red: " #E95678",
-	yellow: " #FAB795"
+		black: "#1C1E26",
+		blue: " #26BBD9",
+		cyan: " #59E1E3",
+		green: " #29D398",
+		magenta: " #EE64AC",
+		red: " #E95678",
+		yellow: " #FAB795",
+		lightBlack: "#232530",
+		lightBlue: "#3FC4DE",
+		lightCyan: "#6BE4E6",
+		lightGreen: "#3FDAA4",
+		lightMagenta: "#F075B5",
+		lightRed: "#EC6A88",
+		lightYellow: "#FBC3A7"
 };
