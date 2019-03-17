b37f13b790f67b4ee4366d559bcad8899aa49b85
Nicholas Morrow <njmorrow95@gmail.com>
2019-03-16 22:48:46 -0400

    Newline before bash prompt in general.

--- general.sh
+++ general.sh
@@ -36,4 +36,4 @@ parse_git_branch() {
 # export PS1='\e[0;35m[\w] $(parse_git_branch) \e[m \e[1;35m$\e[m '
 export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
-export PS1='\[\e[0;36m\]\w\e[m\e[0;35m$(parse_git_branch)\e[m\e[0;32m\n$ \[\e[m\]'
+export PS1='\n\[\e[0;36m\]\w\e[m\e[0;35m$(parse_git_branch)\e[m\e[0;32m\n$ \[\e[m\]'
