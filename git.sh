1d72573d2bf0365c306ce4d679c1403441760481
Nicholas Morrow <njmorrow95@gmail.com>
2019-12-25 16:08:59 -0500

    Remove unused git aliases.

--- git.sh
+++ git.sh
@@ -1,13 +1,11 @@
 # git aliases
 git config --global core.editor "code --wait"
 git config --global alias.fr "!git fetch && git rebase -i origin/master"
-git config --global alias.amend "commit --amend"
-git config --global alias.master "checkout master" 
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.s 'status'
 
 # bash aliases for git
 alias g='git'
-alias gs='git status'
 alias gac='git add -A && git commit -m '
 alias gcom='git checkout master && git pull'
 
