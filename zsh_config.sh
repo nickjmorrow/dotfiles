70072c9c001932a34230a8eea2929c05885dbe85
Nicholas Morrow <njmorrow95@gmail.com>
2022-01-28 16:27:25 -0600

    Add zsh_config.

---
+++ zsh_config.sh
@@ -0,0 +1,35 @@
+source ~/.bashrc
+# If you come from bash you might have to change your $PATH.
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=/usr/local/bin:$PATH
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/nicholasmorrow/.oh-my-zsh
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+  git
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+ZSH_THEME=""
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# added by travis gem
+[ -f /Users/nicholasmorrow/.travis/travis.sh ] && source /Users/nicholasmorrow/.travis/travis.sh
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
+export PROMPT=$'\n%B%F{blue}%1~%b%f $(git_current_branch) %F{magenta}\n>%F{white} '
+
