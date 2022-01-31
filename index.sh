c89d0eb36782c1d1410d8ffb057e43cd0942e538
Nicholas Morrow <nicholas.morrow@tempus.com>
2022-01-31 14:04:23 -0600

    Updated current_dir to work on work laptop.

--- index.sh
+++ index.sh
@@ -2,7 +2,8 @@
 THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
 current_dir=`dirname "${THIS}"`
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# current_dir+="/Projects/dotfiles"
 # current_dir="/Users/nicholasmorrow/Projects/dotfiles"
 
 source "$current_dir/config_access.sh"
